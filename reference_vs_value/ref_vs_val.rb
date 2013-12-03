e2667c3e0fb0ffed431cf5e9e1e39b980d7a38d8
Brandon Conway <brandoncc@gmail.com>
2013-12-03 09:49:03 -0800

    Fixed bug in ref_vs_val after watching intro videos

--- reference_vs_value/ref_vs_val.rb
+++ reference_vs_value/ref_vs_val.rb
@@ -10,7 +10,5 @@ def my_mutating_method array
   array.reverse!
 end
 
-my_mutating_method [0, 1, 2, 3]
+my_mutating_method my_array
 puts my_array
-
-# Array was not affected by either method.  That is because the scope of the variable in the method is not the same as the main scope, where the puts is called.
